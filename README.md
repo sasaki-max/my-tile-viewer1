def5ff3b881435f8c41229b50f1792fcfe8ce1db
sasaki-max <sasaki.sota.v6@s.mail.nagoya-u.ac.jp>
2025-07-09 18:00:49 +0900

    initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+<<<<<<< HEAD
+"# my-tile-viewer" 
+=======
+# my-tile-viewer1
+>>>>>>> 2d5d46382af7987a695b714cac1040ebb177f783
---
+++ script.js
@@ -0,0 +1,35 @@
+const map = L.map('map').setView([34.92, 139.63], 16); // 適宜緯度経度を修正
+
+// 1. 航空写真タイルの読み込み
+L.tileLayer('./tiles/{z}/{x}/{y}.png', {
+  minZoom: 16,
+  maxZoom: 18,
+  attribution: '航空写真'
+}).addTo(map);
+
+// 2. 筆ポリゴンの読み込み
+fetch('fude.geojson')
+  .then(res => res.json())
+  .then(data => {
+    L.geoJSON(data, {
+      style: { color: 'blue', weight: 1, fillOpacity: 0.1 }
+    }).addTo(map);
+  });
+
+// 3. 差分1
+fetch('overlap_result.geojson')
+  .then(res => res.json())
+  .then(data => {
+    L.geoJSON(data, {
+      style: { color: 'red', weight: 2, fillOpacity: 0.3 }
+    }).addTo(map);
+  });
+
+// 4. 差分2
+fetch('overlap_result1.geojson')
+  .then(res => res.json())
+  .then(data => {
+    L.geoJSON(data, {
+      style: { color: 'orange', weight: 2, fillOpacity: 0.3 }
+    }).addTo(map);
+  });
